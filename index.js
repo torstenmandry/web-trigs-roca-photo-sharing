e8e613bd61a91d090a2342955c8f770b41caded1
Torsten Mandry <torsten.mandry@innoq.com>
2021-03-06 10:54:16 +0100

    edit profile via separate page

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import nunjucks from "nunjucks";
+import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import path from "path";
 
 let HOST = "localhost";
 let PORT = 3000;
+
+let PROFILE = {
+    name: 'Torsten Mandry',
+    title: 'Frontend Noob',
+    image: 'https://uploads.innoq.com/4gxstu4sysnzppna6tuj4pqs0qlu?response-content-disposition=inline%3B%20filename%3D%22torsten-mandry-ava-2020-02.jpg%22%3B%20filename%2A%3DUTF-8%27%27torsten-mandry-ava-2020-02.jpg&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHA5PEZW3OX32LYA%2F20210306%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20210306T082922Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=946d6e0d180cc6047ce7086b3c0bb6e7c70c54da6de9c5093377c2b995f1a011'
+}
+
 let IMAGES = [{
     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Gecarcinus_quadratus_%28Nosara%29.jpg/2560px-Gecarcinus_quadratus_%28Nosara%29.jpg",
     description: "Gecarcinus quadratus, a land crab from Central America",
@@ -25,23 +33,29 @@ let IMAGES = [{
     url: "http://de.spongepedia.org/images/Krabs.jpg",
     description: "Mr. Krabs",
     source: "http://de.spongepedia.org/index.php/Mr._Krabs"
-}, {
-    url: "https://static.wikia.nocookie.net/kingdom-hearts/images/d/d6/Sebastian_KHII.png/revision/latest/scale-to-width-down/1000?cb=20130430215503&path-prefix=de",
-    description: "Sebastian",
-    source: "https://kingdomhearts.fandom.com/de/wiki/Sebastian"
 }];
 
 let ROOT = path.dirname(fileURLToPath(import.meta.url));
 
 let app = express();
-app.use("/", express.static(absolutePath("./assets")));
+app.use("/assets", express.static(absolutePath("./assets")));
 nunjucks.configure(absolutePath("./views"), {
 	express: app
 });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.html", { images: shuffle(IMAGES) });
+    res.render("index.html", { images: shuffle(IMAGES), profile: PROFILE });
 });
+app.get("/profile/edit", (req, res) => {
+    res.render("profile-edit.html", { profile: PROFILE });
+})
+app.post("/profile/edit", (req, res) => {
+    let { name, title } = req.body;
+    PROFILE.name = name
+    PROFILE.title = title
+    res.redirect("/");
+})
 
 let server = app.listen(PORT, HOST, _ => {
 	let { address, port } = server.address();
