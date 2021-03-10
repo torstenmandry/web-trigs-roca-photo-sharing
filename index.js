377e97e9410473cd6ab8c23c5c0cdcee5e54bbec
Torsten Mandry <torsten.mandry@innoq.com>
2021-03-10 08:52:09 +0100

    community likes

--- index.js
+++ index.js
@@ -14,25 +14,40 @@ let PROFILE = {
 }
 
 let IMAGES = [{
+    id: 123,
     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Gecarcinus_quadratus_%28Nosara%29.jpg/2560px-Gecarcinus_quadratus_%28Nosara%29.jpg",
     description: "Gecarcinus quadratus, a land crab from Central America",
-    source: "https://en.wikipedia.org/wiki/Crab"
+    source: "https://en.wikipedia.org/wiki/Crab",
+    likes: 17,
+    likedByCurrentUser: true
 }, {
+    id: 124,
     url: "https://upload.wikimedia.org/wikipedia/commons/4/49/Ocypode_quadrata_%28Martinique%29.jpg",
     description: "Ocypode quadrata (Thoracotremata: Ocypodidae)",
-    source: "https://en.wikipedia.org/wiki/Crab"
+    source: "https://en.wikipedia.org/wiki/Crab",
+    likes: 12,
+    likedByCurrentUser: false
 }, {
+    id: 125,
     url: "https://www.abc.net.au/reslib/201006/r576376_3593030.jpg",
     description: "Male fiddler crab watch for other males waving their 'love claw' to detect the presence of a female",
-    source: "https://www.abc.net.au/science/articles/2010/06/02/2915161.htm?site=science/tricks&topic=latest"
+    source: "https://www.abc.net.au/science/articles/2010/06/02/2915161.htm?site=science/tricks&topic=latest",
+    likes: 23,
+    likedByCurrentUser: true
 }, {
+    id: 126,
     url: "https://rasamalaysia.com/wp-content/uploads/2019/12/baked-crab1.jpg",
     description: "Baked Crab",
-    source: "https://rasamalaysia.com/baked-crab-recipe/"
+    source: "https://rasamalaysia.com/baked-crab-recipe/",
+    likes: 0,
+    likedByCurrentUser: false
 }, {
+    id: 127,
     url: "http://de.spongepedia.org/images/Krabs.jpg",
     description: "Mr. Krabs",
-    source: "http://de.spongepedia.org/index.php/Mr._Krabs"
+    source: "http://de.spongepedia.org/index.php/Mr._Krabs",
+    likes: 2,
+    likedByCurrentUser: false
 }];
 
 let ROOT = path.dirname(fileURLToPath(import.meta.url));
@@ -45,7 +60,7 @@ nunjucks.configure(absolutePath("./views"), {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.html", { images: shuffle(IMAGES), profile: PROFILE });
+    res.render("index.html", { images: IMAGES, profile: PROFILE });
 });
 app.get("/profile/edit", (req, res) => {
     res.render("profile-edit.html", { profile: PROFILE });
@@ -56,6 +71,15 @@ app.post("/profile/edit", (req, res) => {
     PROFILE.title = title
     res.redirect("/");
 })
+app.post("/image/:id", (req, res) => {
+    let id = parseInt(req.params.id)
+    let like = parseInt(req.body.like)
+    console.log(`POST /image/${id} - like ${like}`)
+    let image = IMAGES.find(img => img.id === id)
+    image.likes += like
+    image.likedByCurrentUser = like > 0
+    res.redirect("/");
+})
 
 let server = app.listen(PORT, HOST, _ => {
 	let { address, port } = server.address();
@@ -64,8 +88,4 @@ let server = app.listen(PORT, HOST, _ => {
 
 function absolutePath(filepath) {
 	return path.resolve(ROOT, filepath);
-}
-
-function shuffle(array) {
-    return array.sort( () => .5 - Math.random() )
 }
